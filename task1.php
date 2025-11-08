a3b2cd60b506bc267c31678c3fd13a30acab4ed8
MD. Tanvir Sajib (Rohan) <110492334+tanvir-sajib@users.noreply.github.com>
2025-11-08 20:26:13 +0600

    Add files via upload

---
+++ task1.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<head>
+   
+ 
+<title> task1</title>
+</head>
+ 
+   <body style="background-color: aquamarine;">
+   
+<center>
+<h1>Clinic Patient Registration</h1>
+ 
+<table style="padding: 40px;border: 2px ;backgroud-color: white;background-color: lightgray">
+<tr>
+    <td style="padding:10px;">
+     Enter Your Name:
+</td>
+ 
+<td >
+   <input type="text">
+</td>
+</tr>
+ 
+<tr>
+<td style="padding:10px;">    
+   Age:
+</td>
+<td>
+   <input type="text">  
+</td>
+</tr>
+ 
+<tr>
+    <td style="padding:10px;">
+     Phone Number:
+</td>
+ 
+<td >
+   <input type="text">  
+</td>
+</tr>
+ 
+<tr>
+    <td style="padding:10px;">
+     Email Address:
+</td>
+ 
+<td >
+   <input type="text">  
+</td>
+</tr>
+ 
+ 
+    <td style="padding:10px;">
+    Insyrance Provider:
+  </td>
+ 
+  <td >
+    <select>
+      <option>select Provider</option>
+     <option>A</option>
+     <option>B</option>
+     <option>C</option>
+     <option>D</option>
+     </select>
+</td>
+</tr>
+ 
+<tr>
+<td style="padding:10px;">    
+   Insurance Policy Number:
+</td>
+<td>
+   <input type="text">  
+</td>
+</tr>
+ 
+ 
+</table>
+ 
+ <h2>Additional Information</h2>
+ 
+ <table style="padding: 40px;border: 2px ;backgroud-color: white;background-color: lightgray">
+ 
+<tr>
+    <td style="padding:10px;">
+     User name:
+</td>
+ 
+<td >
+   <input type="text">  
+</td>
+</tr>
+ 
+<tr>
+<td style="padding:10px;">    
+   Password:
+</td>
+<td>
+   <input type="text">  
+</td>
+</tr>
+ 
+<tr>
+    <td style="padding:10px;">
+     Confirm Password:
+</td>
+ 
+<td >
+   <input type="text">  
+</td>
+</tr>
+ 
+ <tr>
+    <td style="padding:10px; text-align: right ;">
+ 
+         
+        <input type="submit">
+         
+</td>
+</tr>
+ 
+</table>
+ 
+ 
+ 
+ 
+ 
+</center>
+</body>
